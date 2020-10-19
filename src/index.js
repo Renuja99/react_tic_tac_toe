7a5fa7ca11149f29eb571f73ee56a0090147d0ea
Renuja De Costa <devmindadeco@gmail.com>
2020-10-19 16:19:46 +0530

    Lifting state up

--- src/index.js
+++ src/index.js
@@ -12,13 +12,30 @@ class Square extends React.Component {
     };
   }
   render() {
-    return <button className='square' onClick ={()=>this.setState({value:'X'})}>{this.state.value}</button>;
+  return <button className='square' onClick ={()=>this.props.onClick()}>{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      squares : Array(9).fill(null), 
+    }
+  }
+
+  handleClick(i){
+
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+  
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value ={this.state.squares[i]}
+            onClick={()=> this.handleClick(i)} />;
   }
 
   render() {
